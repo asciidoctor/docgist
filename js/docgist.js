4f4a3039110befaded1215a4393fa1c17486c37a
Anders Nawroth <anders@neotechnology.com>
2013-07-01 10:51:54 +0200

    Cleanup.

--- js/docgist.js
+++ js/docgist.js
@@ -2,6 +2,7 @@ var ASCIIDOCTOR_OPTIONS = Opal.hash2( [ 'attributes' ], {
   'attributes' : [ 'notitle!' ]
 } );
 var DEFAULT_HASH = '#5897167';
+var $IMG = $( '<img>' );
 
 $( window ).hashchange( renderPage );
 
@@ -28,21 +29,15 @@ function renderPage()
     window.history.pushState( {}, "", DEFAULT_HASH );
   }
   var gist = window.location.hash.substr( 1 );
-  // we only allow public gists for now, so the id will be only digits
-  if ( !/^\d+$/.test( gist ) )
-  {
-    // TODO scroll to the correct position as needed?
-    // Probably we should capture the clicks and manage this ourselves somehow.
-    return false;
-  }
-  var url = "https://api.github.com/gists/" + gist;
+  // TODO: filter out anchor references from gist ids (which are hexadecimal)
+  var url = 'https://api.github.com/gists/' + gist;
   $.ajax( {
     url : url,
     success : function( data )
     {
       var file = data.files[Object.keys( data.files )[0]];
       var doc = file.content;
-      $( "#gist-link" ).attr( "href", data.html_url );
+      $( '#gist-link' ).attr( 'href', data.html_url );
       $content = $( '#content' );
       $content.empty();
       var generatedHtml = Opal.Asciidoctor.$render( doc, ASCIIDOCTOR_OPTIONS );
@@ -56,7 +51,7 @@ function renderPage()
           var ext = this.href.split( '.' ).pop();
           if ( 'png|jpg|jpeg|svg'.indexOf( ext ) !== -1 )
           {
-            $link.replaceWith( '<img src="' + this.href + '">' );
+            $link.replaceWith( $IMG.clone().attr( 'src', this.href ) );
           }
         }
       } );
