9be6ac07b26a1a10bcfda73dd42321fc85354cf0
Anders Nawroth <anders@neotechnology.com>
2013-07-09 01:15:13 +0200

    Added support for loading local documents.

--- js/docgist.js
+++ js/docgist.js
@@ -1,23 +1,115 @@
 'use strict';
 
-var ASCIIDOCTOR_OPTIONS = Opal.hash2( [ 'attributes' ], {
-  'attributes' : [ 'notitle!' ]
-} );
-var DEFAULT_GIST = '5897167';
-var $IMG = $( '<img>' );
-var VALID_GIST = /^[0-9a-f]{5,32}$/;
-
-var $content = undefined;
-var $gistId = undefined;
-
-$( document ).ready( function()
+function DocGist( $ )
 {
-  $content = $( '#content' );
-  $gistId = $( '#gist-id' );
+  var ASCIIDOCTOR_OPTIONS = Opal.hash2( [ 'attributes' ], {
+    'attributes' : [ 'notitle!' ]
+  } );
+  var VALID_GIST = /^[0-9a-f]{5,32}$/;
+  //var DEFAULT_SOURCE = '-example';
+  var DEFAULT_SOURCE = '5897167';
 
-  renderPage();
+  var LOCAL_DOCUMENT_SOURCE_ROOT = 'https://github.com/nawroth/docgist/tree/master/gists/';
+  var LOCAL_DOCUMENT_EXTENSION = '.adoc';
 
-  $gistId.keydown( function( event )
+  var $content = undefined;
+  var $gistId = undefined;
+
+  $( document ).ready( function()
+  {
+    $content = $( '#content' );
+    $gistId = $( '#gist-id' );
+
+    renderPage();
+    $gistId.keydown( readSourceId );
+  } );
+
+  function renderPage()
+  {
+    var id = window.location.search;
+    if ( id.length < 2 )
+    {
+      id = DEFAULT_SOURCE;
+    }
+    else
+    {
+      id = id.substr( 1 );
+    }
+    var fetcher = fetchGithubGist;
+    if ( id.length > 1 && id.charAt( 0 ) === '-' )
+    {
+      fetcher = fetchLocalDocument;
+      id = id.substr( 1 );
+    }
+    fetcher( id, renderContent, function( message )
+    {
+      errorMessage( message, id );
+    } );
+  }
+
+  function renderContent( doc, link )
+  {
+    $( '#gist-link' ).attr( 'href', link );
+    $content.empty();
+    var generatedHtml = undefined;
+    try
+    {
+      generatedHtml = Opal.Asciidoctor.$render( doc, ASCIIDOCTOR_OPTIONS );
+    }
+    catch ( e )
+    {
+      errorMessage( e.name + ':' + '<p>' + e.message + '</p>' );
+      return;
+    }
+    $content.html( generatedHtml );
+    $gistId.val( '' );
+  }
+
+  function fetchGithubGist( gist, success, error )
+  {
+    if ( !VALID_GIST.test( gist ) )
+    {
+      error( 'The gist id is malformed: ' + gist );
+      return;
+    }
+
+    var url = 'https://api.github.com/gists/' + gist;
+    $.ajax( {
+      'url' : url,
+      'success' : function( data )
+      {
+        var file = data.files[Object.keys( data.files )[0]];
+        var content = file.content;
+        var link = data.html_url;
+        success( content, link );
+      },
+      'dataType' : 'json',
+      'error' : function( xhr, status, errorMessage )
+      {
+        error( errorMessage );
+      }
+    } );
+  }
+
+  function fetchLocalDocument( id, success, error )
+  {
+    var url = './gists/' + id + '.adoc';
+    $.ajax( {
+      'url' : url,
+      'success' : function( data )
+      {
+        var link = LOCAL_DOCUMENT_SOURCE_ROOT + id + LOCAL_DOCUMENT_EXTENSION;
+        success( data, link );
+      },
+      'dataType' : 'text',
+      'error' : function( xhr, status, errorMessage )
+      {
+        error( errorMessage );
+      }
+    } );
+  }
+
+  function readSourceId( event )
   {
     var $target = $( event.target );
     if ( event.which === 13 || event.which === 9 )
@@ -35,88 +127,24 @@ $( document ).ready( function()
         // for the case a DocGist URL was pasted by mistake!
         gist = gist.substr( 1 );
       }
-      if ( VALID_GIST.test( gist ) )
-      {
-        window.location.assign( '?' + gist );
-      }
-      else
-      {
-        errorMessage( 'The gist id is malformed.', gist );
-      }
+      window.location.assign( '?' + gist );
     }
-  } );
-} );
+  }
 
-function renderPage()
-{
-  var gist = window.location.search;
-  if ( gist.length < 2 )
+  function errorMessage( message, gist )
   {
-    gist = DEFAULT_GIST;
-  }
-  else
-  {
-    gist = gist.substr( 1 );
-  }
-  if ( !VALID_GIST.test( gist ) )
-  {
-    errorMessage( 'The gist id is malformed.', gist );
-    return;
-  }
-  var url = 'https://api.github.com/gists/' + gist;
-  $.ajax( {
-    'url' : url,
-    'success' : function( data )
+    var messageText;
+    if ( gist )
     {
-      var file = data.files[Object.keys( data.files )[0]];
-      var doc = file.content;
-      $( '#gist-link' ).attr( 'href', data.html_url );
-      $content.empty();
-      var generatedHtml = undefined;
-      try
-      {
-        generatedHtml = Opal.Asciidoctor.$render( doc, ASCIIDOCTOR_OPTIONS );
-      }
-      catch ( e )
-      {
-        errorMessage( e.name + ':' + '<p>' + e.message + '</p>' );
-        return;
-      }
-      $content.html( generatedHtml );
-      // transform image links to images
-      $( 'a[href]', $content ).each( function()
-      {
-        var $link = $( this );
-        if ( $link.text() === this.href && this.href.length > 4 )
-        {
-          var ext = this.href.split( '.' ).pop();
-          if ( 'png|jpg|jpeg|svg'.indexOf( ext ) !== -1 )
-          {
-            $link.replaceWith( $IMG.clone().attr( 'src', this.href ) );
-          }
-        }
-      } );
-      $gistId.val( '' );
-    },
-    'dataType' : 'json',
-    'error' : function( xhr, status, error )
-    {
-      errorMessage( error, gist );
+      messageText = 'Somethng went wrong fetching "' + gist + '":<p>' + message + '</p>';
     }
-  } );
+    else
+    {
+      messageText = '<p>' + message + '</p>';
+    }
+
+    $content.html( '<div class="alert alert-block alert-error"><h4>Error</h4>' + messageText + '</div>' );
+  }
 }
 
-function errorMessage( message, gist )
-{
-  var messageText;
-  if ( gist )
-  {
-    messageText = 'Somethng went wrong fetching the gist "' + gist + '":<p>' + message + '</p>';
-  }
-  else
-  {
-    messageText = '<p>' + message + '</p>';
-  }
-
-  $content.html( '<div class="alert alert-block alert-error"><h4>Error</h4>' + messageText + '</div>' );
-}
+DocGist( jQuery );
